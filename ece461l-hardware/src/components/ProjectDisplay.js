819a825f10fa8639311db92ca1415da9708f0d93
shakthip2021 <shakthip2021@utexas.edu>
2023-04-01 19:48:35 -0500

    minor issue with imports

--- ece461l-hardware/src/components/ProjectDisplay.js
+++ ece461l-hardware/src/components/ProjectDisplay.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { TextField } from "@mui/material";
-import Button from "@mui/material/Button";
-import { useState } from 'react';
 
 export default function ProjectDisplay() {
   return (

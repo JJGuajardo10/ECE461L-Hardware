0ef7437316495ef54ebc7dec8f5b1513dbb3a6ee
Josue Guajardo <jjguajardo@utexas.edu>
2023-04-01 19:35:21 -0500

    just making it so it can run

--- ece461l-hardware/src/components/ProjectDisplay.js
+++ ece461l-hardware/src/components/ProjectDisplay.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useState } from 'react';
 
 export default function ProjectDisplay() {
   return (
-     <div className="flex justify-center"></div>
+     <div className="flex justify-center">
+        <h1 id="ProjectsHeader" className='font-mono'>Projects</h1>
+      </div>
   )
 }
 

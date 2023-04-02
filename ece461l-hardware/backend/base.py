cfe63ec2fcdc9df5c82c0ba4fad63e8400d2c285
Benjamin Huynh <98857307+Longshotben@users.noreply.github.com>
2023-04-01 20:50:43 -0500

    Pushing code

--- ece461l-hardware/backend/base.py
+++ ece461l-hardware/backend/base.py
@@ -1,12 +1,31 @@
+import os
+
 from flask import Flask
+from config import Config
+from flask_cors import CORS  # comment this on deployment
+from flask_mongoengine import MongoEngine
+from flask_jwt_extended import JWTManager
 
-api = Flask(__name__)
 
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Josue Guajardo",
-        "about" :"this is just an example to reference"
-    }
+db = MongoEngine() # db initialization occurs before the app starts
 
-    return response_body
+app = Flask(__name__)
+app.config.from_object(Config)
+
+app.config['MONGODB_SETTINGS' ]= {
+    "db": "hardware",
+    "host": "localhost",
+    "port": 5000
+}
+db.init_app(app)
+
+# hw1 = Hardware(name="Set1", capacity=512)
+
+jwt = JWTManager(app)
+
+CORS(app)
+
+from .routes import *
+from .models import init_hardware, init_godmin
+
+init_hardware()
---
+++ ece461l-hardware/backend/models.py
@@ -0,0 +1,49 @@
+from flask_mongoengine import MongoEngine
+
+import pymongo
+
+db = MongoEngine()
+
+
+class User(db.Document):
+    """Application Users - comprised of firstname, lastname, email, and password
+    Fields:
+        first_name: user's first name
+        last_name: user's last name
+        email: user's email (unique identifier)
+        password: user's hashed password
+    """
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True, unique=True)
+    password = db.StringField(required=True)
+
+
+class Projects(db.Document):
+    """Projects for our site. The Data that is associated with the
+    projects
+    Field:
+        name: the name of the project
+        hardware: list of hardware sets associated with this project
+        description: description of the project
+    """
+    name = db.StringField(required=True, min_length=1, max_length=20)
+    description = db.StringField(required=True, min_length=5)
+    hardware = db.ListField(db.DictField())
+    creator_id = db.ObjectIdField(required=True)
+    project_id = db.StringField(required=True, unique=True)
+
+
+class HardwareSet(db.Document):
+    """Hardware Set for checkout
+    Fields:
+        name: the name of the Hardware Set
+        capacity: hardware set capacity (how many we own)
+        available: hardware set availability (how many are not currently checked out)
+    """
+    name = db.StringField(required=True, unique=True, min_length=1)
+    capacity = db.IntField(required=True, min_value=0)
+    available = db.IntField(required=True, min_value=0)
+from models import User, Projects, HardwareSet, db
+
+all = ["db", "User", "Projects", "HardwareSet"]
